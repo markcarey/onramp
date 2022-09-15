b39afda62eecc055dab9d630ca367350f6bb77f9
markcarey <mark@markcarey.com>
2022-09-15 09:43:15 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,10 +6,12 @@ var chain = "mumbai";
 if (chain == "goerli") {
     addr.connext = "0xB4C1340434920d70aD774309C75f9a4B679d801e";
     addr.router = "0xD25575eD38fa0F168c9Ba4E61d887B6b3433F350";
+    addr.test = "0x7ea6eA49B0b0Ae9c5db7907d139D9Cd3439862a1";
 }
 if (chain == "mumbai") {
     addr.connext = zeroAddress;
     addr.router = zeroAddress;
+    addr.test = zeroAddress;
 }
 
 
@@ -18,7 +20,7 @@ async function main() {
     const MyNFT = await ethers.getContractFactory("Rocket");
  
     // Start deployment, returning a promise that resolves to a contract object
-    const myNFT = await MyNFT.deploy(addr.connext, addr.router); // Instance of the contract 
+    const myNFT = await MyNFT.deploy(addr.connext, addr.router, addr.test); // Instance of the contract 
     console.log("Contract deployed to address:", myNFT.address);
  }
  
